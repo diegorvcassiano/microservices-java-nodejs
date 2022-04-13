379a44f068a6b02bfb12f2bde6264e1327424834
diegorvcassiano <diegorvcassiano@gmail.com>
2022-04-13 18:44:39 -0300

    product-api - included a @Trasactional on ProductService validation to revert the trasaction on Database

--- java/product-api/src/main/java/com/drvc/microservice/productapi/services/ProductService.java
+++ java/product-api/src/main/java/com/drvc/microservice/productapi/services/ProductService.java
@@ -17,6 +17,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.util.ObjectUtils;
 
 @Service
@@ -120,6 +121,7 @@ public class ProductService {
 
     }
 
+    @Transactional
     private void validateStockUpdateData(ProductStockDTO productDTO) {
         if (ObjectUtils.isEmpty(productDTO)
                 || ObjectUtils.isEmpty(productDTO.getSalesId())) {
